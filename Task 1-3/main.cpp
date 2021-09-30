f99aabc9c8c76618a34de13f05a75a3ef9b19980
Marina Paley <marina_paley@mail.ru>
2021-09-30 15:14:07 +0300

    Начата работа над 1-3

---
+++ Task 1-3/main.cpp
@@ -0,0 +1,50 @@
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <iostream>
+
+/**
+\brief Рассчитывает объем конуса, вписанного в шар, радиусом R
+\param angle Угол при вершине в осевом сечении конуса
+\param radius Радиус шара
+\return объем конуса
+*/
+double getVolume(double angle, double radius);
+
+/**
+\brief Рассчитывает полную поверхность конуса, вписанного в шар, радиусом R
+\param angle Угол при вершине в осевом сечении конуса
+\param radius Радиус шара
+\return полную поверхность конуса
+*/
+double getSurface(double angle, double radius);
+
+/** 
+\brief Точка входа в программу 
+\return В случае успеха возвращаем 0
+*/
+int main()
+{
+    std::cout << "Введите радиус шара = ";
+    double radius;
+    std::cin >> radius;
+
+    std::cout << "Введите угол при вершине в осевом сечении конуса = ";
+    double angle;
+    std::cin >> angle;
+
+    const double volume = getVolume(angle, radius);
+    const double surface = getSurface(angle, radius);
+    std::cout << "объем конуса, вписанного в шар " << volume;
+    std::cout << "\nполную поверхность конуса, вписанного в шар " << surface << std::endl;
+    return 0;
+}
+
+double getVolume(double angle, double radius)
+{
+    return 0.0;
+}
+
+double getSurface(double angle, double radius)
+{
+    return 0.0;
+}
