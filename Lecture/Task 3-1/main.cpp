3a9d953e9531c6eb01992a9f4673ac676ce23022
Marina Paley <marina_paley@mail.ru>
2021-10-21 16:14:27 +0300

    Сделали задачу 3-1

---
+++ Lecture/Task 3-1/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+
+/**
+ * \brief Рассчитывает значение функции в заданной точке \a x.
+ * \param x Заданная точка.
+ * \return Значение функции в заданной точке \a x.
+ */
+double myFunc(double x);
+
+/**
+ * \brief Узнает, существует ли функция в заданной точке \a x.
+ * \param x Заданная точка.
+ * \return true, если значение функции в заданной точке \a x существует.
+ */
+bool isCalculated(double x);
+
+/**
+ * \brief Точка входа в программу.
+ * \return В случаен успеха код 0.
+ */
+int main()
+{
+    const double X_START = 0.0;
+    const double X_FINISH = 10;
+    const double STEP = 0.1;
+    double x = X_START;
+    while(x <= X_FINISH)
+    {
+        if (isCalculated(x))
+        {
+            const double y = myFunc(x);
+            std::cout << "x = " << std::setw(5) << std::left << std::setprecision(5) << x << " y = " << y << "\n";
+        }
+        else
+        {
+            std::cout << "x = " << x << " y = не существует" << "\n";
+        }
+        x += STEP;
+    }
+    return 0;
+}
+
+double myFunc(const double x)
+{
+    return sqrt(x + 0.75) + x * x - 253.4;
+}
+
+bool isCalculated(const double x)
+{
+    return x >= -0.75;
+}
